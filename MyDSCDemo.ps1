fd71cdfaf6b0b36980bbb462c9315934fa11935b
tliursc <tliursc@hotmail.com>
2015-11-10 11:08:22 +0800

    DSC Demo1

---
+++ MyDSCDemo.ps1
@@ -0,0 +1,14 @@
+﻿Configuration MyDSCDemo
+{
+   Import-DSCResource -Module nx
+   Node "192.168.1.62"{    
+        nxFile myTestFile
+        {
+            Ensure = "Present" 
+            Type = "File"
+            DestinationPath = "/tmp/dsctest"   
+            Contents="This is my DSC Test!"
+        }
+    }
+}
+ 
